5efb06afb7517530991efe9643966c583897e388
Mauricio Miller <mauriciomilleroliveira@hotmail.com>
2025-07-22 13:18:46 -0300

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,6 @@
+﻿preco = float(input('DIGITE O PREÇO DO PRODUTO A SER CALCULADO'))
+p = int(input('DIGITE A PORCENTAGEM DE DESCONTO A SER APLICADA (0-100%)'))
+desconto = preco * (p / 100)
+final = preco - desconto
+print('O Preço BASE é {}\n E o Desconto é de {}%'.format(preco, p))
+print('O VALOR DO DESCONTO FOI DE: {}R$\n O VALOR FINAL É: {}R$'.format(desconto, final))
